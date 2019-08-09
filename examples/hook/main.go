ac6daa4e08ad63471aff80fd957bbfa901f3dfe6
unknown <379342542@qq.com>
2019-08-10 01:06:17 +0800

    feature: add more hooks and support plugin development

--- examples/hook/main.go
+++ examples/hook/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net"
 	"os"
@@ -11,7 +10,7 @@ import (
 	"syscall"
 
 	"github.com/DrmagicE/gmqtt"
-	"github.com/DrmagicE/gmqtt/pkg/packets"
+	"github.com/DrmagicE/gmqtt/plugin/management"
 )
 
 var validUserMu sync.Mutex
@@ -36,6 +35,7 @@ func validateUser(username string, password string) bool {
 }
 
 func main() {
+
 	s := gmqtt.NewServer()
 	ln, err := net.Listen("tcp", ":1883")
 	if err != nil {
@@ -43,75 +43,85 @@ func main() {
 		return
 	}
 
+	s.AddPlugins(management.New(":8080", nil))
+
 	//authentication
-	s.RegisterOnConnect(func(client *gmqtt.Client) (code uint8) {
-		username := client.ClientOptions().Username
-		password := client.ClientOptions().Password
-		fmt.Println("connect")
-		if validateUser(username, password) {
-			return packets.CodeAccepted
-		}
-
-		return packets.CodeBadUsernameorPsw
-	})
-
-	//acl
-	s.RegisterOnSubscribe(func(client *gmqtt.Client, topic packets.Topic) uint8 {
-		if client.ClientOptions().Username == "root" {
-			return topic.Qos
-		}
-		if client.ClientOptions().Username == "qos0" {
-			if topic.Qos <= packets.QOS_0 {
+	/*	s.RegisterOnConnect(func(connect gmqtt.OnConnect) gmqtt.OnConnect {
+			return func(cs gmqtt.ChainStore, client gmqtt.Client) (code uint8) {
+				username := client.ClientOptions().Username
+				password := client.ClientOptions().Password
+				fmt.Println("onConnect")
+				if validateUser(username, password) {
+					return packets.CodeAccepted
+				}
+				return packets.CodeBadUsernameorPsw
+			}
+		})
+		//acl
+		s.RegisterOnSubscribe(func(subscribe gmqtt.OnSubscribe) gmqtt.OnSubscribe {
+			return func(cs gmqtt.ChainStore, client gmqtt.Client, topic packets.Topic) (qos uint8) {
+				if client.ClientOptions().Username == "root" {
+					return topic.Qos
+				}
+				if client.ClientOptions().Username == "qos0" {
+					if topic.Qos <= packets.QOS_0 {
+						return topic.Qos
+					}
+					return packets.QOS_0
+				}
+				if client.ClientOptions().Username == "qos1" {
+					if topic.Qos <= packets.QOS_1 {
+						return topic.Qos
+					}
+					return packets.QOS_1
+				}
+				if client.ClientOptions().Username == "publishonly" {
+					return packets.SUBSCRIBE_FAILURE
+				}
 				return topic.Qos
 			}
-			return packets.QOS_0
-		}
-		if client.ClientOptions().Username == "qos1" {
-			if topic.Qos <= packets.QOS_1 {
-				return topic.Qos
+		})
+
+		s.RegisterOnMsgArrived(func(arrived gmqtt.OnMsgArrived) gmqtt.OnMsgArrived {
+			return func(cs gmqtt.ChainStore, client gmqtt.Client, msg gmqtt.Message) (valid bool) {
+				if client.ClientOptions().Username == "subscribeonly" {
+					client.Close()
+					return false
+				}
+				//Only qos1 & qos0 are acceptable(will be delivered)
+				if msg.Qos() == packets.QOS_2 {
+					return false
+				}
+				return true
 			}
-			return packets.QOS_1
-		}
-		if client.ClientOptions().Username == "publishonly" {
-			return packets.SUBSCRIBE_FAILURE
-		}
-		return topic.Qos
-	})
+		})
+		s.RegisterOnClose(func(onClose gmqtt.OnClose) gmqtt.OnClose {
+			return func(cs gmqtt.ChainStore, client gmqtt.Client, err error) {
+				log.Println("client id:"+client.ClientOptions().ClientID+"is closed with error:", err)
+			}
+		})
 
-	s.RegisterOnUnsubscribed(func(client *gmqtt.Client, topicName string) {
-		log.Printf(`topic: "%s" unsubscribed`, topicName)
-	})
-	s.RegisterOnPublish(func(client *gmqtt.Client, publish *packets.Publish) bool {
-		if client.ClientOptions().Username == "subscribeonly" {
-			client.Close()
-			return false
-		}
-		//Only qos1 & qos0 are acceptable(will be delivered)
-		if publish.Qos == packets.QOS_2 {
-			return false
-		}
-		return true
-	})
+		s.RegisterOnStop(func(stop gmqtt.OnStop) gmqtt.OnStop {
+			return func(cs gmqtt.ChainStore) {
+				log.Println("stop")
+			}
+		})
 
-	s.RegisterOnClose(func(client *gmqtt.Client, err error) {
-		log.Println("client id:"+client.ClientOptions().ClientID+"is closed with error:", err)
-	})
+		s.RegisterOnDeliver(func(deliver gmqtt.OnDeliver) gmqtt.OnDeliver {
+			return func(cs gmqtt.ChainStore, client gmqtt.Client, msg gmqtt.Message) {
+				log.Printf("delivering message %s to client %s", msg.Payload(), client.ClientOptions().ClientID)
+			}
+		})
 
-	s.RegisterOnStop(func() {
-		log.Println("server stopped...")
-	})
-
-	s.RegisterOnDeliver(func(client *gmqtt.Client, publish *packets.Publish) {
-		fmt.Println("ondeliver:", publish)
-	})
-
-	s.RegisterOnAcked(func(client *gmqtt.Client, publish *packets.Publish) {
-		fmt.Println("onAcked:", publish)
-	})
+		s.RegisterOnAcked(func(acked gmqtt.OnAcked) gmqtt.OnAcked {
+			return func(cs gmqtt.ChainStore, client gmqtt.Client, msg gmqtt.Message) {
+				log.Println("msg ack", msg)
+			}
+		})*/
 
 	s.AddTCPListenner(ln)
-	s.Run()
 	log.Println("started...")
+	s.Run()
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
 	<-signalCh
