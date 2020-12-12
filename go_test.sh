7487233f3d14bc52863a5005999f1c28ff680db4
zhanglifang@chinatelecom.cn <379342542@qq.com>
2020-12-12 23:56:33 +0800

    fix ci coverage

---
+++ go_test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+go test -race ./...  -coverprofile=coverage.txt -covermode=atomic
+sed -i -e '/.pb./d' coverage.txt
+sed -i -e '/_mock/d' coverage.txt
+sed -i -e '/example/d' coverage.txt
